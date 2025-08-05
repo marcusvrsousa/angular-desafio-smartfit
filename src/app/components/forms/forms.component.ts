def94d3bab680da0980f69719de7f7d42993f06a
marcusvrsousa <marcus.vrsousa@gmail.com>
2025-08-05 00:14:13 -0300

    feat: create get unit service

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GetUnitsService } from '../../services/get-units.service';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
   results: number[] = [];
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) {}
+
+  ngOnInit() {
+    this.unitService.getUnits().subscribe(response => console.log(response));
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
+
+  onSubmit() {
+    console.log(this.formGroup.value);
+  }
+
+  onClean() {
+    this.formGroup.reset();
+  }
 }
